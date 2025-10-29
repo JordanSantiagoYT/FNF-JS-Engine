4109c7574340dd00304aea6bf8e53a61d09a0cc8
Vinegar (Moxie) <124418090+moxie-coder@users.noreply.github.com>
2025-10-29 11:45:54 -0400

    don't process dead notes

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2824,7 +2824,7 @@ class PlayState extends MusicBeatState
 		{
 			for (group in [notes, sustainNotes])
 				for (note in group){
-					if (note == null)
+					if (note == null || !note.alive)
 						continue;
 					if (ClientPrefs.enableColorShader) note.updateRGBColors();
 				}
@@ -4273,7 +4273,7 @@ class PlayState extends MusicBeatState
 				{
 					for (group in [notes, sustainNotes])
 						for (note in group){
-							if (note == null)
+							if (note == null || !note.alive)
 								continue;
 							if (ClientPrefs.enableColorShader) note.updateRGBColors();
 						}
@@ -4783,7 +4783,7 @@ class PlayState extends MusicBeatState
 
 	function judgeNote(note:Note = null, ?miss:Bool = false)
 	{
-		if (note == null) return;
+		if (note == null || !note.alive) return;
 		if (daRating == null) daRating = ratingsData[0]; //because it likes being stupid
 		if (!cpuControlled)
 		{
@@ -5815,7 +5815,7 @@ class PlayState extends MusicBeatState
 	}
 
 	public function spawnHoldSplashOnNote(note:Note, ?isDad:Bool = false) {
-		if (!ClientPrefs.noteSplashes || note == null)
+		if (!ClientPrefs.noteSplashes || note == null || !note.alive)
 			return;
 
 		splashesPerFrame[(isDad ? 2 : 3)] += 1;
