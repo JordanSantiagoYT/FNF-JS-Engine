c8eb1cef8d1e0966109c99aa2e4da4e94d82c21e
Jordan Santiago <61796448+JordanSantiagoYT@users.noreply.github.com>
2025-12-05 15:10:55 -0500

    uh

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -278,9 +278,9 @@ class MainMenuState extends MusicBeatState
 				changeTipText();
 			}
 		}
-		if (FlxG.sound != null && FlxG.sound.music.volume < 0.8)
+		if (FlxG.sound != null && FlxG.sound?.music?.volume < 0.8)
 		{
-			FlxG.sound.music.volume += 0.5 * FlxG.elapsed;
+			FlxG.sound?.music?.volume += 0.5 * FlxG.elapsed;
 			if(FreeplayState.vocals != null) FreeplayState.vocals.volume += 0.5 * elapsed;
 		}
 
