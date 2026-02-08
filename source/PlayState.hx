0608829c37835026cc6d559db21f3590d9872132
Jordan Santiago <61796448+JordanSantiagoYT@users.noreply.github.com>
2026-02-08 09:59:36 -0500

    Update PlayState.hx?

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -4285,7 +4285,7 @@ class PlayState extends MusicBeatState
 				mid.x - (char == gf ? 0 : char == boyfriend ? 100 : -150),
 				mid.y - (char == gf ? 0 : 100)
 			);
-			camFollow.x += char.cameraPosition[0] + charCamOffset[0];
+			camFollow.x += (char == boyfriend ? -1 : 1) * char.cameraPosition[0] + charCamOffset[0];
 			camFollow.y += char.cameraPosition[1] + charCamOffset[1];
 			if (char == dad || char == gf) tweenCamIn();
 
@@ -6224,3 +6224,4 @@ class PlayState extends MusicBeatState
 		FlxG.autoPause = ClientPrefs.autoPause;
 	}
 }
+
