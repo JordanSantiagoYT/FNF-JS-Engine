90c2e53ca0602e7732c4e21e6d41c68e1cd73ff1
Pato (new acc) <havaianasdestruido@gmail.com>
2026-08-13 13:20:39 -0300

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -3,6 +3,7 @@ color 0a
 cd ..
 echo Installing dependencies...
 echo This might take a few moments depending on your internet speed.
+echo ------------------------------
 @echo on
 haxelib git lime https://github.com/JS-Engine-things/lime-8.1.2 --quiet
 haxelib git openfl https://github.com/JS-Engine-things/openfl --quiet
@@ -23,5 +24,6 @@ haxelib git hxvlc https://github.com/JS-Engine-things/hxvlc --quiet --skip-depen
 haxelib git hxnativefiledialog https://github.com/MAJigsaw77/hxnativefiledialog --quiet --skip-dependencies
 haxelib install hxp
 @echo off
+echo ------------------------------
 echo Finished!
 pause
